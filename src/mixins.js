c264de767fe5b4e0461d6f6207b6c82bf43a7176
Valentin Vago <zeropaper@irata.ch>
2013-11-11 13:54:08 +0100

    add some comments to the mixins

--- src/mixins.js
+++ src/mixins.js
@@ -13,6 +13,77 @@
   'use strict';
   var noop = function(){};
 
+  // provides a method to fetch a resource and its potential
+  // subresources using the soundcloud javascript sdk.
+  function fetch(options) {
+    options       = options || {};
+    /*jshint validthis:true */
+    var instance  = this;
+    var success   = options.success || noop;
+    var error     = options.error || noop;
+    // this allows fetch resources by their `permalink` property
+    // especially usefuly for users
+    var permalink = (instance.get('permalink') || instance.id);
+
+    var apiPath   = '/'+ _.result(instance, 'url') +'/'+ permalink;
+    // fetch the resource when no subresource is specified in the options
+    if (!options.subresource) {
+      SC.get(apiPath, function(data, info) {
+        instance.set(data);
+        success.call(instance, data, info);
+      });
+      return;
+    }
+
+    // alterate the URL to match the subresource endpoint
+    apiPath = apiPath +'/'+ options.subresource;
+
+    // determine the `require` path of the subresource class
+    var subresources = instance.constructor.subresources;
+    var subresource = subresources[options.subresource];
+    if (!subresource) {
+      throw new Error('No subresource found for'+ options.subresource);
+    }
+
+    // load the subresource class AMD module
+    require([subresource], function(Klass) {
+      var subinstance = instance[options.subresource];
+
+      // initialize the subresource instance if needed
+      if (!subinstance) {
+        subinstance = instance[options.subresource] = new Klass();
+
+        // forward subresource change events to the resource instance
+        instance.listenTo(subinstance, 'change', function() {
+          instance.trigger('change:'+ options.subresource, instance, {
+            subresource: options.subresource
+          });
+          instance.trigger('change', instance, {
+            subresource: options.subresource
+          });
+        });
+      }
+
+      // fetch the data with the SC SDK
+      SC.get(apiPath, function(data, info) {
+        // if the subresource is a collection, we use `reset`
+        var method = _.isUndefined(instance.length) ? 'set' : 'reset';
+        subinstance[method](data, {
+          silent: true
+        });
+        subinstance.trigger('change', subinstance, {});
+
+        success.call(instance, data, info);
+      });
+    }, error);
+  }
+
+  // very naive `Backbone.sync` helper to use localStorage
+  // ```js
+  // var Model = Bacbone.Model.extend({
+  //   sync: mixins.localSync('something-meaningful')
+  // });
+  // ```
   function localSync(scope) {
     return function(method, model, options) {
       var success = options.success || noop;
@@ -37,72 +108,7 @@
     };
   }
 
-
-  function fetch(options) {
-    options       = options || {};
-    /*jshint validthis:true */
-    var instance  = this;
-    var success   = options.success || noop;
-    var error     = options.error || noop;
-    var permalink = (instance.get('permalink') || instance.id);
-    console.info('permalink for fetching', permalink, instance.toJSON());
-
-    var apiPath   = '/'+ _.result(instance, 'url') +'/'+ permalink;
-    if (!options.subresource) {
-      SC.get(apiPath, function(data, info) {
-        instance.set(data);
-        success.call(instance, data, info);
-      });
-      return;
-    }
-
-    apiPath = apiPath +'/'+ options.subresource;
-
-    var subresources = instance.constructor.subresources;
-    var subresource = subresources[options.subresource];
-    if (!subresource) {
-      throw new Error('No subresource found for'+ options.subresource);
-    }
-
-    require([subresource], function(Klass) {
-      var subinstance = instance[options.subresource];
-
-      if (!subinstance) {
-        subinstance = instance[options.subresource] = new Klass();
-
-        instance.listenTo(subinstance, 'change', function() {
-          instance.trigger('change:'+ options.subresource, instance, {
-            subresource: options.subresource
-          });
-          instance.trigger('change', instance, {
-            subresource: options.subresource
-          });
-        });
-      }
-
-      SC.get(apiPath, function(data, info) {
-        var method = _.isUndefined(instance.length) ? 'set' : 'reset';
-        subinstance[method](data, {
-          silent: true
-        });
-        subinstance.trigger('change', subinstance, {});
-
-        success.call(instance, data, info);
-      });
-    }, error);
-  }
-
-
-  // function save(attributes, options) {
-  //   options       = options || {};
-  //   var instance  = this;
-  //   var success   = options.success || noop;
-  //   var error     = options.error || noop;
-
-  //   var apiPath = '/'+ _.result(this, 'url') +'/'+ (this.get('username') || this.id);
-
-  // }
-
+  // export the callbacks as an object
   var mixins = {
     fetch: fetch,
     localSync: localSync
