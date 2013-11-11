55455d63bbe74f6d36d62e61518255c6b3aa1994
Valentin Vago <zeropaper@irata.ch>
2013-11-11 23:59:30 +0100

    add view for user

---
+++ src/views/user.js
@@ -0,0 +1,43 @@
+(function(factory) {
+  'use strict';
+  /* global define: true, module: true */
+  // CommonJS
+  if(typeof exports === 'object') {
+    module.exports = factory(require('underscore'), require('backbone'), require('./../templates'));
+  }
+  // AMD
+  else if(typeof define === 'function' && define.amd) {
+    define([
+      'underscore',
+      'backbone',
+      './../templates'
+    ], factory);
+  }
+}(function(_, Backbone, templates) {
+  'use strict';
+
+  // var $ = Backbone.$;
+  var SCUser = Backbone.View.extend({
+    events: {
+    },
+
+    initialize: function(options) {
+      this.router = options.router;
+      this.listenTo(this.model, 'change', this.render);
+    },
+
+    render: function() {
+      var html = '';
+      // only render if we have a loaded resource
+      if (this.model.id) {
+        var data = this.model.toJSON();
+        data.routePrefix = this.router.routePrefix;
+        html = templates['SCBone/user'](data);
+      }
+      this.$el.html(html);
+      return this;
+    }
+  });
+
+  return SCUser;
+}));
